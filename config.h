6bee94ef55de5b4c2b75fde44a8267bb6711ae31
Ibrahim Johar <ibbe.johar@gmail.com>
2022-05-14 23:04:16 +0200

    first commit

---
+++ config.h
@@ -0,0 +1,9 @@
+#define CMDLENGTH 45
+#define DELIMITER "  "
+#define CLICKABLE_BLOCKS
+
+const Block blocks[] = {
+	BLOCK("memory",  1,    0),
+	BLOCK("volume",  0,    5),
+	BLOCK("date +%e/%m'|'%H:%M",    1,    0),
+};
