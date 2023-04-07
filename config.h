e9ffce385bb6c99acc98db694c6598865c44265d
Ibrahim Johar <56348868+ibbejohar@users.noreply.github.com>
2023-04-07 13:57:23 +0200

    Update config.h
    Just change from arch to nixos. Forgott to save last changes.

--- config.h
+++ config.h
@@ -3,8 +3,8 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
+	BLOCK("date +%e/%m'|'%H:%M",    1,    0),
 	BLOCK("mpd-blocks",  1,    0),
 	BLOCK("memory",  10,    0),
 	BLOCK("volume",  0,    5),
-	BLOCK("date +%e/%m'|'%H:%M",    1,    0),
 };
